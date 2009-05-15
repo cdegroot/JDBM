fea88376556710da531c33b31476c3a02f3d4614
Cees de Groot <cdegroot@ebay.com>
2009-05-15 09:40:18 +0200

    mavenization steps

--- cp.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-set cp=%cp%;%1
--- run.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM $Id: run.bat,v 1.2 2003/10/31 00:13:35 dranatunga Exp $
-set JAVA=%JAVA_HOME%\bin\java
-set cp=%CLASSPATH%
-for %%i in (lib\*.jar) do call cp.bat %%i
-set CP=%JAVA_HOME%\lib\tools.jar;build\classes;build\examples;build\tests;%CP%
-rem %JAVA% -Xrunhprof:cpu=samples,depth=10 -classpath %CP% %1 %2 %3 %4 %5 %6
-%JAVA% -classpath %CP% %1 %2 %3 %4 %5 %6
-
